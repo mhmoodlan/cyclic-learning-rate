33edb3e7d67e87add2bd638b2f10ecb5c7d3233e
Mahmoud Aslan <mahmoudaslan@outlook.com>
2018-08-09 17:25:11 +0300

    Add missing imports
    also fixed indention error

--- clr.py
+++ clr.py
@@ -1,3 +1,7 @@
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.eager import context
+
 def cyclic_learning_rate(global_step,
                          learning_rate=0.01,
                          max_lr=0.1,
@@ -79,35 +83,35 @@ def cyclic_learning_rate(global_step,
       rate value across different invocations of optimizer functions.
       @end_compatibility
   """
-   if global_step is None:
+  if global_step is None:
     raise ValueError("global_step is required for cyclic_learning_rate.")
-   with ops.name_scope(name, "CyclicLearningRate",
+  with ops.name_scope(name, "CyclicLearningRate",
                       [learning_rate, global_step]) as name:
     learning_rate = ops.convert_to_tensor(learning_rate, name="learning_rate")
     dtype = learning_rate.dtype
     global_step = math_ops.cast(global_step, dtype)
     step_size = math_ops.cast(step_size, dtype)
-     def cyclic_lr():
+    def cyclic_lr():
       """Helper to recompute learning rate; most helpful in eager-mode."""
       # computing: cycle = floor( 1 + global_step / ( 2 * step_size ) )
       double_step = math_ops.multiply(2., step_size)
       global_div_double_step = math_ops.divide(global_step, double_step)
       cycle = math_ops.floor(math_ops.add(1., global_div_double_step))
-       # computing: x = abs( global_step / step_size – 2 * cycle + 1 )
+      # computing: x = abs( global_step / step_size – 2 * cycle + 1 )
       double_cycle = math_ops.multiply(2., cycle)
       global_div_step = math_ops.divide(global_step, step_size)
       tmp = math_ops.subtract(global_div_step, double_cycle)
       x = math_ops.abs(math_ops.add(1., tmp))
-       # computing: clr = learning_rate + ( max_lr – learning_rate ) * max( 0, 1 - x )
+      # computing: clr = learning_rate + ( max_lr – learning_rate ) * max( 0, 1 - x )
       a1 = math_ops.maximum(0., math_ops.subtract(1., x))
       a2 = math_ops.subtract(max_lr, learning_rate)
       clr = math_ops.multiply(a1, a2)
-       if mode == 'triangular2':
+      if mode == 'triangular2':
         clr = math_ops.divide(clr, math_ops.cast(math_ops.pow(2, math_ops.cast(
             cycle-1, tf.int32)), tf.float32))
       if mode == 'exp_range':
         clr = math_ops.multiply(math_ops.pow(gamma, global_step), clr)
-       return math_ops.add(clr, learning_rate, name=name)
-     if not context.executing_eagerly():
+      return math_ops.add(clr, learning_rate, name=name)
+    if not context.executing_eagerly():
       cyclic_lr = cyclic_lr()
-     return cyclic_lr
+    return cyclic_lr
